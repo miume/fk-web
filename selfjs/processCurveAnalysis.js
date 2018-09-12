1f11c9c1765d3ac0ff47937f47488f3c073af50a
Accumulations <317217599@qq.com>
2018-09-12 15:16:16 +0800

    工艺指标曲线分析模块-完成

--- selfjs/processCurveAnalysis.js
+++ selfjs/processCurveAnalysis.js
@@ -1,14 +1,64 @@
 var averageValues = 0;
-var maxValues = 0;
-var minValues = 0;
+var upValues = 0;
+var downValues = 0;
 var curveAnalysis = {
     init: function() {
         curveAnalysis.funcs.renderLeftOption();
     }
     ,tableName : ""
     ,funcs: {
+        /**渲染初始页面*/
+        renderTable : function() {
+            $.get(home.urls.group.getAll(),{},function(result) {
+                var groups = result.data[0];
+                console.log(groups);
+                var id = groups.id;
+                $('#parameterChart').removeClass('hide');
+                $('#analysisDiv').removeClass('hide');
+                $('#group-'+id).addClass('selected_Item').css('color','#ffffff');
+                $('#group-'+id).addClass('selected_BgItem').css('background','rgb(112,118,139)');
+                // const $tbody = $("#processItemTbody");
+                $.get(home.urls.parameter.getByGroup(),{
+                    groupId:id
+                },function(result) {
+                    var parameters = result.data;
+                    //  获取第一个数据的表名
+                    var firstTableName  = parameters[0]&&parameters[0].group.tableName||'';
+                    //  获取第一个参数id
+                    var firstParameterId = parameters[0]&&parameters[0].id;
+                    //  获取第一个参数的名字
+                    var firstParameterName = parameters[0]&&parameters[0].name;
+                    //  获取当时时间和提前2小时时间
+                    var nowDate = new Date().Format("yyyy-MM-dd hh:mm:ss");
+                    var day = new Date();
+                    var hour = day.getHours() - 2;
+                    var preHour = hour>10 ? hour : '0'+hour ;
+                    var min = day.getMinutes()>10 ? day.getMinutes() : '0'+day.getMinutes() ;
+                    var second = day.getSeconds()>10 ? day.getSeconds() : '0'+day.getSeconds() ;
+                    var nextDate = day.Format("yyyy-MM-dd")+' ' + preHour + ':' + min + ":" + second;
+                    $("#beginDate").val(nowDate);
+                    $("#endDate").val(nextDate);
+                    $("#parameterGroup").append('<option>'+ firstParameterName +'</option>');
+                    $.get(home.urls.parameterData.getByTableNameAndDate(),{
+                        tableName : firstTableName,
+                        startDate : nowDate,
+                        endDate : nextDate,
+                        parameterId : firstParameterId
+                    },function(result) {
+                        var parameterDatas = result.data;
+                        //  获取横坐标
+                        var times = parameterDatas&&parameterDatas.times||'';
+                        //  获取纵左边
+                        var values = parameterDatas&&parameterDatas.values||'';
+                        //  渲染右边分析参数部分
+                        curveAnalysis.funcs.renderRightOption(firstParameterId,times,values);
+                    })
+                })
+
+            })
+        }
         /**渲染左边菜单*/
-        renderLeftOption : function() {
+        ,renderLeftOption : function() {
             $.get(home.urls.group.getAll(),{},function(result) {
                 var groups = result.data;
                 const $tbody = $("#processItemTbody");
@@ -24,11 +74,15 @@ var curveAnalysis = {
                 curveAnalysis.funcs.renderDropData(setGroups); // 选中事件
                 /**绑定开始分析事件 */
                 curveAnalysis.funcs.bindAnalysisEvents($("#analyButton"));
+                /**渲染初始页面 */
+                curveAnalysis.funcs.renderTable();
             })
         }
         /**绑定开始分析事件 */
         ,bindAnalysisEvents : function(buttons) {
             buttons.off('click').on('click',function() {
+                $('#parameterChart').removeClass('hide');
+                $('#analysisDiv').removeClass('hide');
                 var parameterId = $("#parameterGroup").val();
                 var startDate = $("#beginDate").val();
                 var endDate = $("#endDate").val();
@@ -43,15 +97,8 @@ var curveAnalysis = {
                     // parameterId : 8
                 },function(result) {
                     var parameterDatas = result.data;
-                    //  获取横坐标
-                    var times = parameterDatas&&parameterDatas.times||'';
-                    console.log(times);
-                    //  获取纵左边
-                    var values = parameterDatas&&parameterDatas.values||'';
                     //  渲染右边分析参数部分
-                    // curveAnalysis.funcs.renderRightOption(parameterId);
-                    curveAnalysis.funcs.renderRightOption(parameterId,times,values);
-                    // curveAnalysis.funcs.renderCurveChart(times,values);
+                    curveAnalysis.funcs.renderRightOption(parameterId,parameterDatas);
                 })
             })
         }
@@ -59,6 +106,12 @@ var curveAnalysis = {
         /** 渲染下拉框 */
         ,renderDropData : function(setGroups) {
             setGroups.off('click').on('click',function() {
+                $('#parameterChart').addClass('hide');
+                $('#analysisDiv').addClass('hide');
+                $('.setGroup').removeClass('selected_Item').css('color', '');
+                $('.setGroup').removeClass('selected_BgItem').css('background', '');
+                $(this).addClass('selected_Item').css('color','#ffffff');
+                $(this).addClass('selected_BgItem').css('background','rgb(112,118,139)');
                 var id = $(this).attr('id').substr(6);
                 $.get(home.urls.parameter.getByGroup(),{
                     groupId:id
@@ -74,63 +127,62 @@ var curveAnalysis = {
             });
         }
         /**渲染右边菜单*/
-        ,renderRightOption : function(id,times,values) {
+        ,renderRightOption : function(id,parameterDatas) {
+            //  获取横坐标
+            var times = parameterDatas&&parameterDatas.times||'';
+            //  获取纵左边
+            var values = parameterDatas&&parameterDatas.values||'';
             $.get(home.urls.parameter.getById(),{
                 id : id
             },function(result) {
                 var parameters = result.data;
-                averageValues = parameters&&parameters.eps||0;
-                maxValues = parameters&&parameters.upValue||0;
-                minValues = parameters&&parameters.downValue||0;
+                averageValues = parameterDatas&&parameterDatas.average.toFixed(0)||0;
+                upValues = parameters&&parameters.upValue||0;
+                downValues = parameters&&parameters.downValue||0;
                 const $tbody = $("#processAnalysisTbody");
                 $tbody.empty();
                 $tbody.append(
                     "<tr>" +
-                    "<td style='width:60%'>平均值</td>"+
-                    "<td>"+ (parameters&&parameters.eps||0)+"</td>"+
+                    "<td style='width:60%'>期望</td>"+
+                    "<td>"+ (parameterDatas&&parameterDatas.average.toFixed(0)||0)+"</td>"+
                     "</tr>"+
                     "<td style='width:60%'>合格率</td>"+
-                    "<td>"+ ('')+"</td>"+
+                    "<td>"+ (parameterDatas&&parameterDatas.passPercent.toFixed(2)*100||0)+"%</td>"+
                     "</tr>"+
                     "<tr>" +
                     "<td style='width:60%'>最大值</td>"+
-                    "<td>"+ (parameters&&parameters.upValue||0)+"</td>"+
+                    "<td>"+ (parameterDatas&&parameterDatas.max.toFixed(0)||0)+"</td>"+
                     "</tr>"+
                     "<tr>" +
                     "<td style='width:60%'>最小值</td>"+
-                    "<td>"+ (parameters&&parameters.downValue||0)+"</td>"+
+                    "<td>"+ (parameterDatas&&parameterDatas.min.toFixed(0)||0)+"</td>"+
                     "</tr>"+
                     "<td style='width:60%'>最长超上限时间</td>"+
-                    "<td>"+ ('')+"</td>"+
+                    "<td>"+ (parameterDatas&&parameterDatas.maxOverUp.toFixed(0)||0)+"分钟</td>"+
                     "</tr>"+
                     "<td style='width:60%'>累计超上限时间</td>"+
-                    "<td>"+ ('')+"</td>"+
+                    "<td>"+ (parameterDatas&&parameterDatas.sumOverUp.toFixed(0)||0)+"分钟</td>"+
                     "</tr>"+
                     "<td style='width:60%'>最长超下限时间</td>"+
-                    "<td>"+ ('')+"</td>"+
+                    "<td>"+ (parameterDatas&&parameterDatas.maxOverDown.toFixed(0)||0)+"分钟</td>"+
                     "</tr>"+
                     "<td style='width:60%'>累计超下限时间</td>"+
-                    "<td>"+ ('')+"</td>"+
+                    "<td>"+ (parameterDatas&&parameterDatas.sumOverDown.toFixed(0)||0)+"分钟</td>"+
                     "</tr>"
                 );
                 curveAnalysis.funcs.renderCurveChart(times,values);
             });
-            // console.log("--------")
-            // console.log(averageValues);
         }
         /** 绘制曲线图 */
         ,renderCurveChart : function(times,values) {
             var valueLength = values.length;
             var averageValue = [];
-            var maxValue = [];
-            var minValue = [];
+            var upValue = [];
+            var downValue = [];
             for(var i=0;i<valueLength;i++){
                 averageValue.push(averageValues);
-                maxValue.push(maxValues);
-                minValue.push(minValues);
-                // averageValue.push(2.2);
-                // maxValue.push(2.4);
-                // minValue.push(1.9);
+                upValue.push(upValues);
+                downValue.push(downValues);
             };
             var myChart = echarts.init(document.getElementById('parameterChart'));
             var option = {
@@ -172,11 +224,11 @@ var curveAnalysis = {
                     type : 'line'
                 },{
                     name: '上限',
-                    data : maxValue,
+                    data : upValue,
                     type : 'line'
                 },{
                     name: '下限',
-                    data : minValue,
+                    data : downValue,
                     type : 'line'
                 }]
             };
@@ -186,17 +238,5 @@ var curveAnalysis = {
                 myChart.resize();
             }
         }
-        /**绑定选中事件*/
-        // ,changeGroup : function(setGroups) {
-        //     setGroups.off('click').on('click',function() {
-        //         var id = $(this).attr('id').substr(6);
-        //         // console.log(id);
-        //
-        //     })
-        // }
-        /**渲染右边事件*/
-        // ,renderRight : function() {
-        //
-        // }
     }
 };
